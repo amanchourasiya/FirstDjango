65d238bbe7bde71b44a95c1f7a474f8980bcd0ad
Aman Chourasiya <amanchourasia6@gmail.com>
2018-09-25 08:52:34 -0700

    Added views

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(response):
     return HttpResponse('Hey Got the first app running')
+
+def detail(request,question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request,question_id):
+    response = "you are looking at the response of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
